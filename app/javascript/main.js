f6e5b5af20b6250d80d22128ab723e7d88695076
ak-matsu <sandbox.matsu@gmail.com>
2021-07-10 16:37:34 +0900

    Switch文を使わない方法

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,22 +1,22 @@
 function check() {
 
   const btn = document.getElementById('btn');
-
+  const result = ['大吉','中吉','凶']
   btn.addEventListener('click', () => {
     const n = Math.floor(Math.random() * 3);
-    // btn.textContent = n;
+    btn.textContent = result[n];
 
-    switch (n) {
-      case 0:
-        btn.textContent = '大吉';
-        break;
-      case 1:
-        btn.textContent = '中吉';
-        break;
-      case 2:
-          btn.textContent = '凶';
-        break;
-    }
+    // switch (n) {
+    //   case 0:
+    //     btn.textContent = '大吉';
+    //     break;
+    //   case 1:
+    //     btn.textContent = '中吉';
+    //     break;
+    //   case 2:
+    //     btn.textContent = '凶';
+    //     break;
+    // }
 
   });
 }
