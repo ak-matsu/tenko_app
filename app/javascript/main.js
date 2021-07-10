33d26098f19d78fb14d925b641a436989d69c504
ak-matsu <sandbox.matsu@gmail.com>
2021-07-10 14:56:48 +0900

    ランダム関数を使い0~3までボタンを押したら表示させるよう記述

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -3,7 +3,8 @@ function check() {
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', () => {
-    btn.textContent = 'hit!';
+    const n = Math.floor(Math.random() * 3);
+    btn.textContent = n;
   });
 }
   window.addEventListener("load", check);
