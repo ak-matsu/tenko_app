9f248472d98af47f67891addc29023305a625264
ak-matsu <sandbox.matsu@gmail.com>
2021-07-10 19:11:04 +0900

    html.erbにClassを入れてcssに色分けするも反応がない。

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -5,11 +5,17 @@ function check() {
   btn.addEventListener('click', () => {
     const n = Math.random();
     if (n < 0.05) {
-      btn.textContent = "大吉"; //5%
+      btn.textContent = '大吉'; //5%
+      btn.classList.add('daikiti');
+
     }else if (n < 0.2){
       btn.textContent = '中吉'; //15%
+      btn.classList.add('chukiti');
+
     }else{
       btn.textContent = '凶'; //80%
+      btn.classList.add('kyo');
+
     }
   });
 }
