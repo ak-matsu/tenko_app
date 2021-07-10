288f932d78f5928f9f09f75fbc8cebfaf7bc694a
ak-matsu <sandbox.matsu@gmail.com>
2021-07-10 16:39:29 +0900

    配列の要素が追加されてもランダム値を都度変更しなくて済むようにresult.lengthを記述

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,9 +1,9 @@
 function check() {
 
   const btn = document.getElementById('btn');
-  const result = ['大吉','中吉','凶']
+  const result = ['大吉','中吉','凶','末吉']
   btn.addEventListener('click', () => {
-    const n = Math.floor(Math.random() * 3);
+    const n = Math.floor(Math.random() * result.length);
     btn.textContent = result[n];
 
     // switch (n) {
