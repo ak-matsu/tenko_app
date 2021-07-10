9a87097a3144118100a6569c36f1f85d2b681ac5
ak-matsu <sandbox.matsu@gmail.com>
2021-07-10 16:48:29 +0900

    コメント文の削除

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -3,21 +3,7 @@ function check() {
   const btn = document.getElementById('btn');
   const result = ['大吉','中吉','凶','末吉']
   btn.addEventListener('click', () => {
-    const n = Math.floor(Math.random() * result.length);
-    btn.textContent = result[n];
-
-    // switch (n) {
-    //   case 0:
-    //     btn.textContent = '大吉';
-    //     break;
-    //   case 1:
-    //     btn.textContent = '中吉';
-    //     break;
-    //   case 2:
-    //     btn.textContent = '凶';
-    //     break;
-    // }
-
+    btn.textContent = result[Math.floor(Math.random() * result.length)];
   });
 }
   window.addEventListener("load", check);
