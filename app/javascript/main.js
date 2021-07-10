308895988fff28b7b3aaaccc356b870fd5323e26
ak-matsu <sandbox.matsu@gmail.com>
2021-07-10 16:54:30 +0900

    if文を使った確率操作

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,9 +1,16 @@
 function check() {
 
   const btn = document.getElementById('btn');
-  const result = ['大吉','中吉','凶','末吉']
+
   btn.addEventListener('click', () => {
-    btn.textContent = result[Math.floor(Math.random() * result.length)];
+    const n = Math.random();
+    if (n < 0.05) {
+      btn.textContent = "大吉"; //5%
+    }else if (n < 0.2){
+      btn.textContent = '中吉'; //15%
+    }else{
+      btn.textContent = '凶'; //80%
+    }
   });
 }
   window.addEventListener("load", check);
